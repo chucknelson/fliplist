d5cba20cc024e759d85f0a0a0f4cf4b44fdf36cb
Chuck Nelson <nelson.charles@gmail.com>
2020-08-12 18:56:03 +0000

    Change global script to js...again

--- app/javascript/global.coffee
+++
@@ -1,6 +0,0 @@
-# Place all the global behaviors and hooks here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
-
-$(document).on "turbolinks:load", ->
-	$('#flash').delay('5000').slideUp('400')
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -15,11 +14,11 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-require("jquery")
-require("jquery-ujs")
-require("jquery-ui/ui/widgets/sortable")
-require("turbolinks").start()
+require('jquery')
+require('jquery-ujs')
+require('jquery-ui/ui/widgets/sortable')
+require('turbolinks').start()
 
-require("../global")
-require("../items")
-require("../lists")
+require('../global')
+require('../items')
+require('../lists')
