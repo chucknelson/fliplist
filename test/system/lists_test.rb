8abcef69e581611ca47608d712337c398803db43
Chuck Nelson <nelson.charles@gmail.com>
2020-08-24 23:42:07 +0000

    Stopping point for first system test

--- test/system/lists_test.rb
+++ test/system/lists_test.rb
@@ -1,9 +1,16 @@
 require 'application_system_test_case'
 
 class ListsTest < ApplicationSystemTestCase
-  test 'visiting the index' do
-    visit lists_url
+  test 'create a new item' do
+    visit login_url
+    fill_in 'Email', with: 'user1@email.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log In'
 
-    assert_current_path login_path
+    test_list = lists(:user1_list1)
+    visit user_list_url test_list.user_id, test_list.id
+    assert_current_path user_list_path test_list.user_id, test_list.id
+
+    #...more
   end
 end
