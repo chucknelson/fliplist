1d5624c43993af7843ac9459a172d7af13b60545
Chuck Nelson <nelson.charles@gmail.com>
2020-09-02 03:12:36 +0000

    Add failing test for issue with edit item textbox

--- test/system/lists_test.rb
+++ test/system/lists_test.rb
@@ -1,16 +1,25 @@
 require 'application_system_test_case'
 
 class ListsTest < ApplicationSystemTestCase
-  test 'create a new item' do
-    visit login_url
-    fill_in 'Email', with: 'user1@email.com'
-    fill_in 'Password', with: 'password'
-    click_button 'Log In'
+  setup { puts 'doing some setup!' }
+
+  test 'edit an item name' do
+    sign_in(users(:user1))
 
     test_list = lists(:user1_list1)
-    visit user_list_url test_list.user_id, test_list.id
-    assert_current_path user_list_path test_list.user_id, test_list.id
+    test_item = items(:user1_list1_item3)
 
-    #...more
+    # Go to list
+    visit user_list_url test_list.user_id, test_list.id
+    sleep 2
+
+    # Edit item
+    find('a', text: test_item.name).sibling('a.item-edit-link').click
+    item_text_input = find_field("item-#{test_item.id}-name")
+    item_text_input.click
+    assert has_focus?(item_text_input), 'Item text input not selected' # <= if not true, problem!
+    sleep 1
+    item_text_input.send_keys(:backspace, :backspace, :backspace) # <= this works even if the typical "click + type" doesn't...
+    assert puts has_focus?(item_text_input)
   end
 end
