2d42d274129f52e7bec0210fd8da105fe82978c8
Chuck Nelson <nelson.charles@gmail.com>
2020-09-05 02:30:58 +0000

    Add item edit system test

--- test/system/lists_test.rb
+++ test/system/lists_test.rb
@@ -1,9 +1,11 @@
 require 'application_system_test_case'
 
 class ListsTest < ApplicationSystemTestCase
-  setup { puts 'doing some setup!' }
+  setup do
+    # Nothing yet
+  end
 
-  test 'edit an item name' do
+  test 'Edit an item' do
     sign_in(users(:user1))
 
     test_list = lists(:user1_list1)
@@ -11,15 +13,19 @@ class ListsTest < ApplicationSystemTestCase
 
     # Go to list
     visit user_list_url test_list.user_id, test_list.id
-    sleep 2
 
-    # Edit item
-    find('a', text: test_item.name).sibling('a.item-edit-link').click
+    # Get item references - its link and its edit text input field
+    item_link = find('a', text: test_item.name)
+    item_link.sibling('a.item-edit-link').click
     item_text_input = find_field("item-#{test_item.id}-name")
+
+    # User can click and get focus / type in it
     item_text_input.click
-    assert has_focus?(item_text_input), 'Item text input not selected' # <= if not true, problem!
-    sleep 1
-    item_text_input.send_keys(:backspace, :backspace, :backspace) # <= this works even if the typical "click + type" doesn't...
-    assert puts has_focus?(item_text_input)
+    assert has_focus?(item_text_input), "Item text input can't be selected"
+
+    # User can update item as expected
+    item_text_input.send_keys(' updated')
+    click_link('Done')
+    assert item_link.text == test_item.name + ' updated'
   end
 end
